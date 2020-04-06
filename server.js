0da3b37cea16eac3728d0c65a974ceb7ba224564
Cecilie <cfog19@student.aau.dk>
2020-04-06 13:12:32 +0200

    Fikset noget stof

--- server.js
+++ server.js
@@ -18,22 +18,24 @@ connection.connect((err) => {
     console.log('Connected!');
 });
 
-let id = 6;
-connection.query(`SELECT * FROM \`Elev\` WHERE \`Id\` = ${id}`, function (err, result, fields) {
+let id = 1;
+connection.query(`SELECT * FROM \`Opgaver\` WHERE \`Opgave-ID\` = ${id}`, function (err, result) {
     if (err) throw err;
     console.log(result);
 });
+
+//SELECT `Opgave-ID` FROM `Opgaver` WHERE 1
 
 
 // Adds index.html as a frontpage, when opening server
 app.get('/',(req, res) =>{
     res.sendFile(__dirname + '/index.html');
 });
-app.get('/Opgaver/index.html',(req, res) =>{
-    res.sendFile(__dirname + '/Opgaver/index.html');
+app.get('/Opgaver/opgaver.html',(req, res) =>{
+    res.sendFile(__dirname + '/Opgaver/opgaver.html');
 });
-app.get('/Opgaver/opgave.html',(req, res) =>{
-    res.sendFile(__dirname + '/Opgaver/opgave.html');
+app.get('/Opgaver/opgavesaet.html',(req, res) =>{
+    res.sendFile(__dirname + '/Opgaver/opgavesaet.html');
 });
 app.get('/oversigt/laereroversigt.html',(req, res) =>{
     res.sendFile(__dirname + '/oversigt/laereroversigt.html');
@@ -42,8 +44,13 @@ app.get('/oversigt/Oversigt.html',(req, res) =>{
     res.sendFile(__dirname + '/oversigt/Oversigt.html');
 });
 
-
 app.listen(3000, () => {
     console.log('Server is up and running at port 3000');
 });
 
+
+
+// Vi skal have lavet en funktion der henter opgaven ned
+    // Klik på pil går videre til næste opgave
+
+// Funktion der sender svar til database
