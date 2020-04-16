ed104bef82ee473378163b4649c813d9fcda99ea
Piite <36481436+Piiit3@users.noreply.github.com>
2020-04-16 11:14:13 +0200

    Update opgaver.js

--- testapp/routes/opgaver.js
+++ testapp/routes/opgaver.js
@@ -26,7 +26,7 @@ router.get('/:opgavenummer', function(req, res, next) {
         res.render('opgaver', {  
             opgavebeskrivelse: result[0].Opgave,
             opgave_ID: result[0].Opgave_ID,
-            
+            svgrad: result[0].Forventet_svar,
             });
       });  
 
