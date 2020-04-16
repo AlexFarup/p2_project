ef26afec8fd34f471f285678418df46dc6cfca17
Piite <36481436+Piiit3@users.noreply.github.com>
2020-04-16 11:40:49 +0200

    Update opgaver.js

--- testapp/routes/opgaver.js
+++ testapp/routes/opgaver.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var environment = require('../enviroment').environment;
 
-
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 /* GET home page. */
 router.get('/:opgavenummer', function(req, res, next) {
@@ -33,19 +33,26 @@ router.get('/:opgavenummer', function(req, res, next) {
   });
 
 
-  router.post('/sendA', function(req, res, next) {
-    
-    var svar     = req.body.svar;
-    
-    
-  var sql = `INSERT INTO \`Besvarelser\`(\`Besvarelse_ID\`, \`Tid\`, \`svar\`, \`Elev_ID\`)`; VALUES `([svar],[svar],[svar],[svar)`;
-   db.query(sql,function (err, data) {
-      if (err) throw err;
-           console.log("record inserted");
-       });
-   res.redirect('/opgaver/:opgavenummer');
+  app.get('/opgaver', function (req, res) {
+    res.render('opgaver', { qs: req.query });
+});
 
-  });
+app.post('/sendA', urlencodedParser, function (req, res) {
+    console.log(req.body);
+    var svar = req.body.svar;
+ 
+    con.connect(function (err) {
+        if (err) throw err;
+        console.log("connected");
+
+        var sql = `INSERT INTO \`Besvarelser\`(\`Besvarelse_ID\`, \`Tid\`, \`svar\`, \`Elev_ID\`)`; VALUES ([svar],[svar],[svar],[svar]);
+        con.query(sql, function (err) {
+            if (err) throw err;
+            console.log("One record inserted");
+        });
+    });
+    res.render('opgaver', { data: req.body });
+});
 
 
 });
