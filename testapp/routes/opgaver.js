66423db3da87f6b9bc6b6ff21d11c5fc3d0b6982
Piite <36481436+Piiit3@users.noreply.github.com>
2020-04-16 11:58:09 +0200

    Update opgaver.js

--- testapp/routes/opgaver.js
+++ testapp/routes/opgaver.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var environment = require('../enviroment').environment;
+var bodyParser = require('body-parser');
 
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 /* GET home page. */
 router.get('/:opgavenummer', function(req, res, next) {
@@ -32,7 +34,9 @@ router.get('/:opgavenummer', function(req, res, next) {
   });
 
 
- 
+  app.get('/opgaver', function (req, res) {
+    res.render('opgaver', { qs: req.query });
+});
 
 app.post('/sendA', urlencodedParser, function (req, res) {
     console.log(req.body);
@@ -42,7 +46,7 @@ app.post('/sendA', urlencodedParser, function (req, res) {
         if (err) throw err;
         console.log("connected");
 
-        var sql = `INSERT INTO Besvarelser(Besvarelse_ID, Tid, svar, Elev_ID) VALUES ('svar','svar','svar','svar')`;
+        var sql = `INSERT INTO \`Besvarelser\`(\`Besvarelse_ID\`, \`Tid\`, \`svar\`, \`Elev_ID\`)`; VALUES ('[svar]','[svar]','[svar]','[svar]');
         con.query(sql, function (err) {
             if (err) throw err;
             console.log("One record inserted");
