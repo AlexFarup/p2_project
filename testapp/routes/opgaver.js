62c521eb5d05bd4faba0e19bade9aba35a2cefc1
Piite <36481436+Piiit3@users.noreply.github.com>
2020-04-22 19:53:53 +0200

    ejtu5

--- testapp/routes/opgaver.js
+++ testapp/routes/opgaver.js
@@ -21,12 +21,30 @@ router.get('/:opgavenummer', function(req, res, next) {
   con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
-    con.query(`SELECT * FROM \`Opgaver\` WHERE \`Opgave_ID\` = ${opgavenummer}`, function (err, result, fields) {
+    con.query(`SELECT *
+    FROM \`Elev\` AS elev
+        INNER JOIN
+            \`Laerer\` AS laerer
+            ON elev.Elev_laerer_ID = laerer.Laerer_ID
+        INNER JOIN
+            \`Klasse\` AS klasse
+            ON elev.Elev_klasse_ID = klasse.Klasse_ID
+        INNER JOIN
+            \`Besvarelser\` AS besvarelser
+            ON elev.Elev_ID = besvarelser.Besv_Elev_ID   
+        INNER JOIN
+        \`Opgaver\` AS opgaver
+          ON besvarelser.Besvarelse_ID = opgaver.Opgave_ID    
+        WHERE
+            elev.Elev_ID = ${opgavenummer}`, function (err, result, fields) {
         if (err) throw err;
         console.log(result[0]);
         res.render('opgaver', {  
-            opgavebeskrivelse: result[0].Opgave,
+            opgavebeskrivelse: result[0].opg.Opgave,
             opgave_ID: result[0].Opgave_ID,
+
+            besvarelse_svar: result[0].Besv_Svar,
+            forventet_svar: result[0].opg_Forv_svar
             
             });
       });  
@@ -63,6 +81,20 @@ router.post('/sendA/:opgavenummer', function (req, res) {
       });
   });
   res.redirect(`back`);
+
+  function compare(besvarelse_svar, forventet_svar) {
+    if (besvarelse_svar == forventet_svar) {
+      `INSERT INTO \`Besvarelser\` (\`Besvarelse_ID\`, \`Besv_Tid\`, \`Besv_Svar\`, \`Besv_Hint\`, \`Besv_Score\`, \`Besv_Elev_ID\`, \`Best_Besvaret\`) VALUES (${opgavenummer}, '0', ${svar}, '0' '1' '1' '1')`
+    }
+    if (besvarelse_svar > forventet_svar) {
+      `INSERT INTO \`Besvarelser\` (\`Besvarelse_ID\`, \`Besv_Tid\`, \`Besv_Svar\`, \`Besv_Hint\`, \`Besv_Score\`, \`Besv_Elev_ID\`, \`Best_Besvaret\`) VALUES (${opgavenummer}, '0', ${svar}, '0' '0' '1' '1')`
+    }
+    else
+    `INSERT INTO \`Besvarelser\` (\`Besvarelse_ID\`, \`Besv_Tid\`, \`Besv_Svar\`, \`Besv_Hint\`, \`Besv_Score\`, \`Besv_Elev_ID\`, \`Best_Besvaret\`) VALUES (${opgavenummer}, '0', ${svar}, '0' '0' '1' '1')`
+  }
+
+
+
 });
 
 
--- testapp/routes/oversigt.js
+++ testapp/routes/oversigt.js
@@ -52,8 +52,6 @@ router.get('/:opgavenummer', function(req, res, next) {
           laerer_telefonnummer: result[0].Laerer_telefonnummer,
           laerer_email: result[0].Laerer_email,
           klasse_navn: result[0].Navn,
-          besvarelse_svar: result[0].Besv_Svar,
-          forventet_svar: result[0].opg_Forv_svar
       });
     });
   });
@@ -87,17 +85,6 @@ router.post('/sendA/:opgavenummer', function (req, res) {
       });
   });
   res.redirect(`back`);
-
-  function compare(besvarelse_svar, forventet_svar) {
-    if (besvarelse_svar == forventet_svar) {
-      `INSERT INTO \`Besvarelser\` (\`Besvarelse_ID\`, \`Besv_Tid\`, \`Besv_Svar\`, \`Besv_Hint\`, \`Besv_Score\`, \`Besv_Elev_ID\`, \`Best_Besvaret\`) VALUES (${opgavenummer}, '0', ${svar}, '0' '1' '1' '1')`
-    }
-    if (besvarelse_svar > forventet_svar) {
-      `INSERT INTO \`Besvarelser\` (\`Besvarelse_ID\`, \`Besv_Tid\`, \`Besv_Svar\`, \`Besv_Hint\`, \`Besv_Score\`, \`Besv_Elev_ID\`, \`Best_Besvaret\`) VALUES (${opgavenummer}, '0', ${svar}, '0' '0' '1' '1')`
-    }
-    else
-    `INSERT INTO \`Besvarelser\` (\`Besvarelse_ID\`, \`Besv_Tid\`, \`Besv_Svar\`, \`Besv_Hint\`, \`Besv_Score\`, \`Besv_Elev_ID\`, \`Best_Besvaret\`) VALUES (${opgavenummer}, '0', ${svar}, '0' '0' '1' '1')`
-  }
 
 });
 
