0c2c7e1336aa13abf05f784d1f266a9ae7e95c62
Piite <36481436+Piiit3@users.noreply.github.com>
2020-04-22 23:10:38 +0200

    Update opgaver.js

--- testapp/routes/opgaver.js
+++ testapp/routes/opgaver.js
@@ -22,7 +22,22 @@ router.get('/:opgavenummer', function(req, res, next) {
   con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
-    con.query(`SELECT * FROM \`Opgaver\` WHERE \`Opgave_ID\` = ${opgavenummer}`, function (err, result, fields) {
+    con.query(`SELECT *
+    FROM \`Elev\` AS elev
+        INNER JOIN
+            \`Laerer\` AS laerer
+            ON elev.Elev_laerer_ID = laerer.Laerer_ID
+        INNER JOIN
+            \`Klasse\` AS klasse
+            ON elev.Elev_klasse_ID = klasse.Klasse_ID
+        INNER JOIN
+            \`Besvarelser\` AS besvarelser
+            ON elev.Elev_ID = besvarelser.Besv_Elev_ID   
+        INNER JOIN
+        \`Opgaver\` AS opgaver
+          ON besvarelser.Besvarelse_ID = opgaver.Opgave_ID    
+        WHERE
+            elev.Elev_ID = ${opgavenummer}`, function (err, result, fields) {
         if (err) throw err;
         console.log(result[0]);
         res.render('opgaver', {  
