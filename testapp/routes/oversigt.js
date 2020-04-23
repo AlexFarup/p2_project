5f0a465d47085a98eae5d9ec0cd8a743b080038b
Piite <36481436+Piiit3@users.noreply.github.com>
2020-04-23 02:32:37 +0200

    Update oversigt.js

--- testapp/routes/oversigt.js
+++ testapp/routes/oversigt.js
@@ -51,36 +51,47 @@ router.get('/:opgavenummer', function(req, res, next) {
           laerer_efternavn: result[0].Laerer_efternavn,
           laerer_telefonnummer: result[0].Laerer_telefonnummer,
           laerer_email: result[0].Laerer_email,
-          klasse_navn: result[0].Navn
+          klasse_navn: result[0].Navn,
+          besvarelse_svar: result[0].Besv_Svar,
+          forventet_svar: result[0].opg_Forv_svar
       });
     });
   });
-
-  con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    con.query(`SELECT *
-    FROM \`Elev\` AS elev
-        INNER JOIN
-            \`Laerer\` AS laerer
-            ON elev.Elev_laerer_ID = laerer.Laerer_ID
-        INNER JOIN
-            \`Klasse\` AS klasse
-            ON elev.Elev_klasse_ID = klasse.Klasse_ID
-        INNER JOIN
-            \`Besvarelser\` AS besvarelser
-            ON elev.Elev_ID = besvarelser.Besvarelse_ID   
-        INNER JOIN
-        \`Opgaver\` AS opgaver
-          ON besvarelser.Besvarelse_ID = opgaver.Opgave_ID    
-        WHERE
-            elev.Elev_ID = ${opgavenummer}`, function (err, result, fields) {
-      if (err) throw err;
-      console.log(result[0]);
-      res.render('oversigt', { })
-    });
-
 });
+
+router.post('/sendA/:opgavenummer', function (req, res) {
+  console.log(req.body);
+  var svar = req.body.svar;
+  var opgavenummer = req.params.opgavenummer;
+  var mysql = require('mysql');
+
+  
+  var con = mysql.createConnection({
+    host: environment.host,
+    user: environment.user,
+    password: environment.password,
+    database: environment.database,
+    port: environment.port,
+    multipleStatements: true
+  });
+
+  con.connect(function (err) {
+    if (err) throw err;
+    console.log("connected");
+
+  function compare(besvarelse_svar, forventet_svar) {
+    if (besvarelse_svar == forventet_svar) {
+      var sql = `INSERT INTO \`Besvarelser\` (\`Besvarelse_ID\`, \`Besv_Tid\`, \`Besv_Svar\`, \`Besv_Hint\`, \`Besv_Score\`, \`Besv_Elev_ID\`, \`Besv_Besvaret\` ) VALUES (${opgavenummer}, '0', ${svar}, '0', '1', '1', '1')`;
+    }
+    if (besvarelse_svar > forventet_svar) {
+      var sql = `INSERT INTO \`Besvarelser\` (\`Besvarelse_ID\`, \`Besv_Tid\`, \`Besv_Svar\`, \`Besv_Hint\`, \`Besv_Score\`, \`Besv_Elev_ID\`, \`Besv_Besvaret\` ) VALUES (${opgavenummer}, '0', ${svar}, '0', '0', '1', '1')`;
+    }
+    else
+    var sql = `INSERT INTO \`Besvarelser\` (\`Besvarelse_ID\`, \`Besv_Tid\`, \`Besv_Svar\`, \`Besv_Hint\`, \`Besv_Score\`, \`Besv_Elev_ID\`, \`Besv_Besvaret\` ) VALUES (${opgavenummer}, '0', ${svar}, '0', '0', '1', '1')`;
+  }
+
+  });
+  res.redirect(`back`);
 
 });
 
