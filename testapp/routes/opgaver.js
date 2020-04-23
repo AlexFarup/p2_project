5170a3c70447aa9d72867c0db2c5563666afdd5a
Piite <36481436+Piiit3@users.noreply.github.com>
2020-04-23 13:53:41 +0200

    Update opgaver.js

--- testapp/routes/opgaver.js
+++ testapp/routes/opgaver.js
@@ -64,7 +64,7 @@ router.post('/sendA/:opgavenummer', function (req, res) {
   con.connect(function (err) {
       if (err) throw err;
       console.log("connected");
-      let sql = 0;
+      let sql = 
       function compare(besvarelse_svar, forventet_svar) {
         if (besvarelse_svar == forventet_svar) {
         sql = `UPDATE \`Besvarelser\` (\`Besvarelse_ID\`, \`Besv_Tid\`, \`Besv_Svar\`, \`Besv_Hint\`, \`Besv_Score\`, \`Besv_Elev_ID\`, \`Besv_Besvaret\` ) VALUES (${opgavenummer}, '0', ${svar}, '0', '1', '1', '1')`;
