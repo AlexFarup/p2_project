e158f74d71b04eead465e8caf5b8aa1beb879c5b
Piite <36481436+Piiit3@users.noreply.github.com>
2020-04-23 12:13:28 +0200

    Update opgaver.js

--- testapp/routes/opgaver.js
+++ testapp/routes/opgaver.js
@@ -65,7 +65,7 @@ router.post('/sendA/:opgavenummer', function (req, res) {
       if (err) throw err;
       console.log("connected");
 
-      var sql = "UPDATE Besvarelser (Besv_Svar) VALUES ('${svar}')";
+      var sql = "UPDATE Besvarelser SET Besv_Svar = '${svar}' WHERE Besvarelse_ID = '${opgavenummer}'";
       con.query(sql, function (err) {
           if (err) throw err;
           console.log("One record inserted");
