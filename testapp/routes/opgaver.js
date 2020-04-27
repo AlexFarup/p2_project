1af9d88677d2ea4f1bea89479e81b0b57d766b52
Piite <36481436+Piiit3@users.noreply.github.com>
2020-04-27 10:28:27 +0200

    Update opgaver.js

--- testapp/routes/opgaver.js
+++ testapp/routes/opgaver.js
@@ -49,8 +49,8 @@ router.post('/sendA/:opgavenummer', function (req, res) {
   var svar = req.body.svar;
   var opgavenummer = req.params.opgavenummer;
   var mysql = require('mysql');
-  let a = `${svar}`;
-  let forventet_svar;
+  let a = 4;
+  let forventet_svar = 4;
   
   
   var con = mysql.createConnection({
@@ -88,7 +88,7 @@ router.post('/sendA/:opgavenummer', function (req, res) {
             var sql = `UPDATE \`Besvarelser\` SET \`Besv_Score\` = 0 WHERE \`Besvarelse_ID\` = ${opgavenummer}`;
           }
 
-          
+
       });  
       
   });
