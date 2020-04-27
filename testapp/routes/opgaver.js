c208042caa032dd5ef4553492cfbe4420f2272c4
Piite <36481436+Piiit3@users.noreply.github.com>
2020-04-27 09:40:44 +0200

    Update opgaver.js

--- testapp/routes/opgaver.js
+++ testapp/routes/opgaver.js
@@ -65,38 +65,36 @@ router.post('/sendA/:opgavenummer', function (req, res) {
   con.connect(function (err) {
       if (err) throw err;
       console.log("connected");
-     
-   
-      var sql = `UPDATE \`Besvarelser\` SET \`Besv_Svar\` = ${svar} WHERE \`Besvarelse_ID\` = ${opgavenummer}`;
-      var sql = `UPDATE \`Besvarelser\` SET \`Besv_Score\` = ${svar} WHERE \`Besvarelse_ID\` = ${opgavenummer}`;
-      con.query(sql, function (err) {
-          if (err) throw err;
-          console.log("One record inserted");
-      });  
-  });
-  res.redirect(`back`);
 
 
-function compare(besvarelse_svar, forventet_svar) {
-  if (besvarelse_svar == forventet_svar) {
-    var sql = `UPDATE \`Besvarelser\` SET \`Besv_Score\` = \`1\` WHERE \`Besvarelse_ID\` = ${opgavenummer}`;
-    con.query(sql, function (err) {
-        if (err) throw err;
-        console.log("One record inserted");
-  });
-  
-  if (besvarelse_svar != forventet_svar){
-    var sql = `UPDATE \`Besvarelser\` SET \`Besv_Score\` = \`0\` WHERE \`Besvarelse_ID\` = ${opgavenummer}`;
-    con.query(sql, function (err) {
-        if (err) throw err;
-        console.log("One record inserted");
-    });
-  }
-}
-}
+      var a = $svar;
+      var b = forventet_svar;
+
+      function compare(a, b) {
+        if (a == b) {
+          var sql = `UPDATE \`Besvarelser\` SET \`Besv_Score\` = \`1\` WHERE \`Besvarelse_ID\` = ${opgavenummer}`;
+          con.query(sql, function (err) {
+              if (err) throw err;
+              console.log("One record inserted");
+        });
+        
+        if (a != b){
+          var sql = `UPDATE \`Besvarelser\` SET \`Besv_Score\` = \`0\` WHERE \`Besvarelse_ID\` = ${opgavenummer}`;
+          con.query(sql, function (err) {
+              if (err) throw err;
+              console.log("One record inserted");
+          });
+        }
+      }
+      }
+
+
+
+
+
 
 });
 
-
+});
 
 module.exports = router;
