365588a1a4af364a61742e5e9e6849b6b027a741
Piite <36481436+Piiit3@users.noreply.github.com>
2020-04-27 10:57:46 +0200

    Update opgaver.js

--- testapp/routes/opgaver.js
+++ testapp/routes/opgaver.js
@@ -69,8 +69,7 @@ router.post('/sendA/:opgavenummer', function (req, res) {
       
 
       if (`${svar}` == forventet_svar) {
-        var sql = `UPDATE \`Besvarelser\` SET \`Besv_Svar\` = ${svar} WHERE \`Besvarelse_ID\` = ${opgavenummer}`;
-        var sql = `UPDATE \`Besvarelser\` SET \`Besv_Score\` = 1 WHERE \`Besvarelse_ID\` = ${opgavenummer}`;
+        var sql = `UPDATE \`Besvarelser\` SET \`Besv_Svar\`, \`Besv_Score\` = 1, ${svar} WHERE \`Besvarelse_ID\` = ${opgavenummer}`;
       }
       else  {
         var sql = `UPDATE \`Besvarelser\` SET \`Besv_Svar\` = ${svar} WHERE \`Besvarelse_ID\` = ${opgavenummer}`;
