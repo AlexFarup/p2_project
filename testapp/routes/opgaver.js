5753c812c146451c27241f769665154f1608b6a8
Piite <36481436+Piiit3@users.noreply.github.com>
2020-04-29 11:38:25 +0200

    Update opgaver.js

--- testapp/routes/opgaver.js
+++ testapp/routes/opgaver.js
@@ -49,7 +49,7 @@ router.post('/sendA/:opgavenummer', function (req, res) {
   var svar = req.body.svar;
   var opgavenummer = req.params.opgavenummer;
   var mysql = require('mysql');
- var forventet_svar = req.params.opg_Forv_svar;
+ var forventet_svar = req.params.opgaver.opg_Forv_svar;
   
   var con = mysql.createConnection({
     host: environment.host,
