d944911e15a302a8a9dc11a9a957825e3e11a54a
Piite <36481436+Piiit3@users.noreply.github.com>
2020-05-14 04:53:31 +0200

    Update opgaver.js

--- testapp/routes/opgaver.js
+++ testapp/routes/opgaver.js
@@ -83,13 +83,13 @@ router.post('/sendA/:opgavenummer', function (req, res) {
         if (hintpoint == 0 && tid_score < forventet_tid){
           score = score;
         }
-        else if (hintpoint == 0 && tid_score > forventet_tid){
+        if (hintpoint == 0 && tid_score > forventet_tid){
           score = score/2;
         }
-        else if (hintpoint == 1 && tid_score < forventet_tid){
+        if (hintpoint == 1 && tid_score < forventet_tid){
           score = score/2;
         }
-        else if (hintpoint == 1 && tid_score > forventet_tid){
+        if (hintpoint == 1 && tid_score > forventet_tid){
           score = score/4;
         }
       }
