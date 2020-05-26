8bf31e768b8fd3f33882a1ca045e413bdf19ea89
Piite <36481436+Piiit3@users.noreply.github.com>
2020-05-26 15:29:55 +0200

    Update opgaver.js

--- testapp/routes/opgaver.js
+++ testapp/routes/opgaver.js
@@ -127,120 +127,110 @@ router.post('/sendA/:opgavenummer', function (req, res) {
 //--------------------------Alex
 
 let data = [
-    [100, 10, 100], // opgave 1 svg 1
-
-    [25, 100, 30], // opgave 2 svg 3
-
-    [60, 50, 75], // opgave 3 svg 2
-
-    [100, 11, 100], // opgave 4 svg 1
-
-    [100, 9, 95], // opgave 5 svg 1
-
-    [100, 8, 100], // opgave 6 svg 1
-
-    [100, 10, 100], // opgave 7 svg 1
-
-    [55, 120, 70], // opgave 8 svg 2
-
-    [22, 200, 25], // opgave 9 svg 3
-
-    [55, 90, 70], // opgave 10 svg 2 
-
-    [20, 200, 25], // opgave 11 svg 3
+    [1, 0.01, 1], // opgave 1 svg 1
+  
+    [0.25, 1.5, 0.30], // opgave 2 svg 3
+  
+    [0.60, 0.50, 0.75], // opgave 3 svg 2
+  
+    [1, 0.11, 1], // opgave 4 svg 1
+  
+    [1, 0.09, 0.95], // opgave 5 svg 1
+  
+    [1, 0.08, 1], // opgave 6 svg 1
+  
+    [1, 0.10, 1], // opgave 7 svg 1
+  
+    [0.55, 1.20, 0.70], // opgave 8 svg 2
+  
+    [0.22, 2.00, 0.25], // opgave 9 svg 3
+  
+    [0.55, 0.90, 0.70], // opgave 10 svg 2 
+  
+    [0.20, 2.00, 0.25], // opgave 11 svg 3
     
-    [95, 4, 95], // opgave 12 svg 1
-
-    [100, 7, 100], // opgave 13 svg 1
-
-    [50, 95, 65], // opgave 14 svg 2 
-
-    [100, 8, 100], // opgave 15 svg 1 
-
-    [15, 220, 20] // opgave 16 svg 3
-
-];
-
-
-// let kPlaceringer = let kPlaceringer
-// let mindsteAfstand = let mindsteAfstand
-// let dataExtremer = let dataExtremer
-// let dataRaekkevidde = let dataRaekkevidde
-
-let kPlaceringer = [];
-let mindsteAfstand = [];
-let dataExtremer;
-let dataRaekkevidde;
-
-let samlingSvaerhedsGrader = [];
-
-function kMeansUdregning(){
-
-    mindsteAfstand.length = 0;
-    kPlaceringer.length = 0;
-
-    dataExtremer = skafDataExtremer();
-    dataRaekkevidde = skafDataRaekkevidde(dataExtremer);
-    kPlaceringer = initialiseringK();
+    [0.95, 0.04, 0.95], // opgave 12 svg 1
+  
+    [1, 0.07, 1], // opgave 13 svg 1
+  
+    [0.50, 0.95, 0.65], // opgave 14 svg 2 
+  
+    [1, 0.08, 1], // opgave 15 svg 1 
+  
+    [0.15, 2.20, 0.20] // opgave 16 svg 3
+  
+  ];
+  
+  
+  
+  kMeansUdregning();
+  
+  function kMeansUdregning(){
+      
     
-    koer();
-    svaerhedsGrad();
+    let dataExtremer = skafDataExtremer();
+    let dataRaekkevidde = skafDataRaekkevidde(dataExtremer);
+    let kPlaceringer = initialiseringK(3, dataRaekkevidde, dataExtremer);
     
-}
-
-
-// Her definerer hvilke datapunkter udgøre vores extremer/grænser
-
-function skafDataExtremer(){
+    erPunktetFlyttet(kPlaceringer, dataExtremer, dataRaekkevidde);
+    
+    
+  };
+  
+  
+  // Her definerer hvilke datapunkter udgøre vores extremer/grænser
+  
+  function skafDataExtremer(){
     let extremer = [];
-
+  
     for (let dataPunkt = 0; dataPunkt < data.length; dataPunkt++){
         let punkt = data[dataPunkt];
-
+  
         // I den første if sætning kontrollere vi om extremer arrayet på den i'ete plads er defineret
         // altså om den har nogen værdi. Hvis ikke, så giver vi den en minimums og max grænse
         for (let XYZ = 0; XYZ < punkt.length; XYZ++){
             if (!extremer[XYZ]) {
                 extremer[XYZ] = { min: 1000, max: 0 };
             }
-
+  
             // Her definerer vi en minimumsgrænse
             if (punkt[XYZ] < extremer[XYZ].min){
                 extremer[XYZ].min = punkt[XYZ];
             }
-
+  
             // Her definere vi en maksimumsgrænse
             if (punkt[XYZ] > extremer[XYZ].max){
                 extremer[XYZ].max = punkt[XYZ];
             }
         }
     }
-
+  
     return extremer;
-}
-
-// Finder området vi kigger på i henholdsvis X og Y aksen
-function skafDataRaekkevidde(extremer){
+  }
+  
+  // Finder området vi kigger på i henholdsvis X og Y aksen
+  function skafDataRaekkevidde(dataExtremer){
     let raekkevidde = [];
-
-    for (let XYZ = 0; XYZ < extremer.length; XYZ++){
-        raekkevidde[XYZ] = extremer[XYZ].max - extremer[XYZ].min;
+  
+    for (let XYZ = 0; XYZ < dataExtremer.length; XYZ++){
+        raekkevidde[XYZ] = dataExtremer[XYZ].max - dataExtremer[XYZ].min;
     }
-
+  
     return raekkevidde;
-}
-
-function initialiseringK(k){
+  }
+  
+  function initialiseringK(k, dataRaekkevidde, dataExtremer){
+    let kPlaceringer = [];
     // Hvis k ikke har en værdi sættes den til 3,
     // da vi 3 rangeringer vi gerne vil finde
     if (k === undefined) {
         k = 3;
     }
-
+  
     // Finder et X og Y koordinat til hver k punkt
     while (k > 0){
         let kPlads = [];
-
+  
         // .min bruges her da vi gerne vil finde en k placering indenfor vores område
         // defineret i vores forrige funktion.
         // Havde det været max ville k blive placeret uden for vores rækkevidde,
@@ -248,28 +238,28 @@ function initialiseringK(k){
         for (let XYZ = 0; XYZ < dataExtremer.length; XYZ++){
             kPlads[XYZ] = dataExtremer[XYZ].min + Math.random() * dataRaekkevidde[XYZ];
         }
-
+  
         kPlaceringer.push(kPlads);
-
+  
         k--;
     }
-
+  
   return kPlaceringer;
-}
-
-function euclids(){
+  }
+  
+  function euclids(mindsteAfstand, kPlaceringer){
     // Yderste for-løkke tager et X og Y koordinatet fra et punkt i datasæt
     for (let dataPunkt = 0; dataPunkt < data.length; dataPunkt++){
         let punkt = data[dataPunkt];
         let afstande = [];
-
+  
         // Tager et k punkts koordinater og placere i letiabel
         // Dette gøres for at vi næste for-løkke kan finde afstandende mellem
         // alle datapunkterne og de 3 k clusters
         for (let kKoordinater = 0; kKoordinater < kPlaceringer.length; kKoordinater++){
             let kPlads = kPlaceringer[kKoordinater];
             let sum = 0;
-
+  
             // Her findes afstanden mellem de førnævnte datapunkter og k clusters
             // Alle datapunkterne sammenlignes med et k cluster af gangen
             // Forskellen summeres op
@@ -278,37 +268,39 @@ function euclids(){
                 forskel *= forskel;
                 sum += forskel;
             }
-
+  
             // Her fuldføres euclids formlen ved at tage kvadratroden af summen
             afstande[kKoordinater] = Math.sqrt(sum);
         }
-
+  
         // Finder indexet på den k værdi der har den mindste afstand til punktet
         // og lægger i et array (skal bruges senere)
         mindsteAfstand[dataPunkt] = afstande.indexOf(Math.min.apply(null, afstande));  
     }
-}
-
-function flytKpunkt(){
+    
+  }
+  
+  function flytKpunkt(kPlaceringer, dataExtremer, dataRaekkevidde){
+    let mindsteAfstand = [];
     //Kalder euclids funktion for at finde mindsteafstanden fra hvert datapunkt til hvert k punkt
-    euclids();
-
+    euclids(mindsteAfstand, kPlaceringer);
+    
     // Vi tager means arrayets længde og laver sums om til et array med samme længde (Means indeholder alle de tilfældige X, Y og Z værdier for centralpunkterne)
     let summer = Array(kPlaceringer.length);
     // Vi tager means arrayets længde og laver sums om til et array med samme længde (Means indeholder alle de tilfældige X, Y og Z værdier for centralpunkterne)
     let counter = Array(kPlaceringer.length);
     let flyttet = false;
-
+  
     //Vi forbereder summer til at kunne blive til et to-dimensionelt array
     //Det bliver lavet til et array med et array inden i
     for (let kKoordinater = 0; kKoordinater < kPlaceringer.length; kKoordinater++){
         
         counter[kKoordinater] = 0;
-
+  
         // Her laves der plads til et X og et Y koordinat på hvert felt i
         // summer arrayet eksempel summer[[X, Y, Z], [X, Y, Z], [X, Y, Z]]
         summer[kKoordinater] = Array(kPlaceringer[kKoordinater].length);
-
+  
         // For at sikre at vi ved hvilke værdier der kommer ind i summer arrayet
         // initialiseres alle felterne til 0
         // summer [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
@@ -316,7 +308,7 @@ function flytKpunkt(){
             summer[kKoordinater][XYZ] = 0;
         }
     }
-
+  
     for (let punkt_index = 0; punkt_index < mindsteAfstand.length; punkt_index++){
         //Han flytter datapunkterne over i en varibel enkeltvis ligesom ovenstående
         let punkt = data[punkt_index];
@@ -327,7 +319,7 @@ function flytKpunkt(){
         //Distancen (fra datapunkt til k = mindsteAfstand = mean_index) bruges til at vælge et punkt i counts arrayet som han tæller op
         //Counts sørger for at skubbe k punkterne *****
         counter[k_index]++;
-
+  
     
         //I første iteration er mean index første k punkt og dimension er X værdien
         //Lægger XYZ værdien fra datapunktet (point[dimension]) over i summer som i første iteration er 0
@@ -335,12 +327,12 @@ function flytKpunkt(){
             summer[k_index][XYZ] += punkt[XYZ];
         }
     } 
-
+  
     for (let k_index = 0; k_index < summer.length; k_index++){
         //Kontrollere at alle k punkter har et datapunkt associeret med det
         if (0 === counter[k_index]){
             summer[k_index] = kPlaceringer[k_index];
-
+  
             // Hvis det viser sig at et k punkt ikke har nogle datapunkter findes der en ny tilfældig placering
             for (let XYZ = 0; XYZ < dataExtremer.length; XYZ++){
                 summer[k_index][XYZ] =
@@ -348,7 +340,7 @@ function flytKpunkt(){
             }
             continue;
         }
-
+  
         // Vi itere igennem 2D arrayet i summer, hvor vi første finder gennemsnitsafstanden fra hvert k punkt til datapunktet for at vi kan flytte k punktet ind i midten af datapunkterne
         // Arrayet ville se således ud:
         // Sum [
@@ -361,34 +353,53 @@ function flytKpunkt(){
             summer[k_index][XYZ] /= counter[k_index];
         }
     }
-
+  
     // Hvis vores k placering er forskelligt fra vores summer, så betyder det at vores k punkt har flyttet sig
     if (kPlaceringer.toString() !== summer.toString()){
         flyttet = true;
-
+  
     }
-
+  
     // Hvis k punktet er forskelligt fra vores summer, betyder det at vi ikke er færdige endnu, og vores summer kan derfor blive vores nye k placeringer
     kPlaceringer = summer;
-
-    return flyttet;
-}
-
-// Denne funktion sørger for at køre vores k-means algoritme indtil k punkterne ikke længere flytter sig
-function koer(){
-    let flyttet = flytKpunkt();
-    if (flyttet !== false){
-        koer();
+  
+    let slutteligeVaerdier = {
+      flyttet: flyttet,
+      mindsteAfstand: mindsteAfstand,
+      kPlaceringer: kPlaceringer
+    };
+    
+    
+    return slutteligeVaerdier;
+  }
+  
+  // Denne funktion sørger for at køre vores k-means algoritme indtil k punkterne ikke længere flytter sig
+  function erPunktetFlyttet(kPlaceringer, dataExtremer, dataRaekkevidde){
+      
+    let slutteligeVaerdier = flytKpunkt(kPlaceringer, dataExtremer, dataRaekkevidde);
+    
+    if (slutteligeVaerdier.flyttet !== false){
+        kPlaceringer = slutteligeVaerdier.kPlaceringer;
+        
+        erPunktetFlyttet(kPlaceringer, dataExtremer, dataRaekkevidde);    
     }
-}
-
-function svaerhedsGrad() { 
-
-      //nulstiller alle elementpladser i samlingSvaerhedsgrader, da vi ikke ved hvad der er paa pladserne  
-      for(let tilknyttetCentralpunk = 0; tilknyttetCentralpunk < data.length; tilknyttetCentralpunk++){
-        samlingSvaerhedsGrader[tilknyttetCentralpunk] = 0;
+    // Den sidste if forhindre den rekursive funktion i at foresætte
+    if(slutteligeVaerdier.flyttet === false) {
+        svaerhedsGrad(slutteligeVaerdier.kPlaceringer, slutteligeVaerdier.mindsteAfstand);
+        
     }
-
+    
+  }
+  
+  function svaerhedsGrad(kPlaceringer, mindsteAfstand) { 
+    let samlingSvaerhedsGrader = [];
+    
+    
+    //nulstiller alle elementpladser i samlingSvaerhedsgrader, da vi ikke ved hvad der er paa pladserne  
+    for(let tilknyttetCentralpunkt = 0; tilknyttetCentralpunkt < data.length; tilknyttetCentralpunkt++){
+        samlingSvaerhedsGrader[tilknyttetCentralpunkt] = 0;
+    }
+  
     //Koerer igennem alle centralpunkterne
     for(let centralpunkt = 0; centralpunkt  < kPlaceringer.length; centralpunkt++){
         let x = 0;
@@ -397,20 +408,20 @@ function svaerhedsGrad() {
         let taeller = 0;
         let antalKoordinater = 3;
         let svaerhedsgrad = 1;
-
-
-        //Tjekker centralpunkterets x, y og z-værdi ved foeste svaerhedsgrad
+  
+  
+        //Tjekker centralpunkterets x, y og z-værdi ved foerste svaerhedsgrad
         if(taeller === 0){
-
+  
             //Der koeres igennem x, y og z koordinaterne for hvert centralpunkt
             for(let XYZ = 0; XYZ < antalKoordinater; XYZ++){
-
+  
                 //Tjekker x-koordinatet
                 if(XYZ === 0){
                     if(kPlaceringer[centralpunkt][XYZ] >= 0.9){
                         x++;
                     } 
-
+  
                 //Tjekker y-koordinatet
                 }else if(XYZ === 1){
                     if(kPlaceringer[centralpunkt][XYZ] < 0.12){
@@ -419,24 +430,24 @@ function svaerhedsGrad() {
                 
                 //Tjekker z-koordinatet
                 }else if(XYZ === 2 ){
-                    if(kPlaceringer[centralpunkt][XYZ] >= 0.13){
+                    if(kPlaceringer[centralpunkt][XYZ] >= 0.90){
                         z++;
                     }
                 }
-
+  
                 //Hvis har fundet 2 eller flere koordinater der passer
-                if((x + y + z) >= 2){
+                if((x + y + z) === 3){
                     
                     
                     //forløkken koerer saa laenge der er daatapunkter
-                    for(let tilknyttetCentralpunk = 0; tilknyttetCentralpunk < data.length; tilknyttetCentralpunk++){
-
+                    for(let tilknyttetCentralpunkt = 0; tilknyttetCentralpunkt < data.length; tilknyttetCentralpunkt++){
+  
                         //Tjekker om opgaven er associeret med centralunktet
-                        if(mindsteAfstand[tilknyttetCentralpunk] === centralpunkt){
-
+                        if(mindsteAfstand[tilknyttetCentralpunkt] === centralpunkt){
+  
                             //Skubber svarhedsgraden op, hvis ovenstaaende passer 
-                            samlingSvaerhedsGrader[tilknyttetCentralpunk] = svaerhedsgrad;
-
+                            samlingSvaerhedsGrader[tilknyttetCentralpunkt] = svaerhedsgrad;
+  
                             //taeller taelles op, saa der ikke bliver tjekket for flere svaerhedsgrader
                             taeller++;
                         }
@@ -444,47 +455,47 @@ function svaerhedsGrad() {
                 } 
             }  
         }
-
+  
      
         //Tjekker centralpunkterets x, y og z-værdi ved naeste svaerhedsgrad
         if(taeller === 0) {
-
+  
             //Taeller svaerhedsgraden op, så der nu tjekkes for svarhedsgrad 2
             svaerhedsgrad++;
-
+  
             //Der koeres igennem x, y og z koordinaterne for hvert centralpunkt
             for(let XYZ = 0; XYZ < antalKoordinater; XYZ++){
-
+  
                 //Tjekker x-koordinatet
                 if(XYZ === 0){
-                    if(kPlaceringer[centralpunkt][XYZ] >= 0.25 && kPlaceringer[centralpunkt][XYZ]/100 <= 0.89){
+                    if(kPlaceringer[centralpunkt][XYZ] >= 0.40 && kPlaceringer[centralpunkt][XYZ] <= 0.89){
                         x++;
                     } 
-
+  
                 //Tjekker y-koordinatet
                 }else if(XYZ === 1){
-                    if(kPlaceringer[centralpunkt][XYZ] >= 0.12 && kPlaceringer[centralpunkt][XYZ]/100 < 1.6){
+                    if(kPlaceringer[centralpunkt][XYZ] >= 0.12 && kPlaceringer[centralpunkt][XYZ] <= 1){
                         y++;
                     }
-
+  
                 //Tjekker z-koordinatet
                 }else if (XYZ === 2){
-                    if(kPlaceringer[centralpunkt][XYZ] >= 0.13 && kPlaceringer[centralpunkt][XYZ]/100 <= 1){
+                    if(kPlaceringer[centralpunkt][XYZ] >= 0.50 && kPlaceringer[centralpunkt][XYZ] <= 0.89){
                         z++;
                     }
                 }
                 //Hvis har fundet 2 eller flere koordinater der passer
-                if((x + y + z) >= 2){
-
+                if((x + y + z) === 3){
+  
                     //forløkken koerer saa laenge der er daatapunkter
-                    for(let tilknyttetCentralpunk = 0; tilknyttetCentralpunk < data.length; tilknyttetCentralpunk++){
-
+                    for(let tilknyttetCentralpunkt = 0; tilknyttetCentralpunkt < data.length; tilknyttetCentralpunkt++){
+  
                         //Tjekker om opgaven er associeret med centralunktet
-                        if(mindsteAfstand[tilknyttetCentralpunk] === centralpunkt){
-
+                        if(mindsteAfstand[tilknyttetCentralpunkt] === centralpunkt){
+  
                             //Skubber svarhedsgraden op, hvis ovenstaaende passer 
-                            samlingSvaerhedsGrader[tilknyttetCentralpunk] = svaerhedsgrad;
-
+                            samlingSvaerhedsGrader[tilknyttetCentralpunkt] = svaerhedsgrad;
+  
                             //taeller taelles op, saa der ikke bliver tjekket for flere svaerhedsgrader
                             taeller++;
                         }
@@ -497,30 +508,27 @@ function svaerhedsGrad() {
       
         //Tjekker centralpunkterets x, y og z-værdi ved naeste svaerhedsgrad
         if(taeller === 0) {
-
+  
             //Taeller svaerhedsgraden op, så der nu tjekkes for svarhedsgrad 3
             svaerhedsgrad++;
-
+  
            
-
+  
             //forløkken koerer saa laenge der er daatapunkter
-            for(let tilknyttetCentralpunk = 0; tilknyttetCentralpunk < data.length; tilknyttetCentralpunk++){
-
+            for(let tilknyttetCentralpunkt = 0; tilknyttetCentralpunkt < data.length; tilknyttetCentralpunkt++){
+  
                 //Tjekker om opgaven er associeret med centralunktet
-                if(mindsteAfstand[tilknyttetCentralpunk] === centralpunkt){
-
+                if(mindsteAfstand[tilknyttetCentralpunkt] === centralpunkt){
+  
                     //Skubber svarhedsgraden op, hvis ovenstaaende passer 
-                    samlingSvaerhedsGrader[tilknyttetCentralpunk] = svaerhedsgrad;
-
+                    samlingSvaerhedsGrader[tilknyttetCentralpunkt] = svaerhedsgrad;
+  
                     //taeller taelles op, saa der ikke bliver tjekket for flere svaerhedsgrader
                     taeller++;
                 }
             }
         }
     } 
-
+    console.log(samlingSvaerhedsGrader);
     return samlingSvaerhedsGrader;
-}
-
-
-module.exports = router;
+  }
